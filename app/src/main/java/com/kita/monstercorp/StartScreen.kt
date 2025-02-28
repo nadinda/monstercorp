bb02f4b40b298b92a0a6609ecc260bb9b43a12eb
Nadinda <nadinda@nadinda.dev>
2025-02-28 21:03:03 +1100

    add background gradient

--- app/src/main/java/com/kita/monstercorp/StartScreen.kt
+++ app/src/main/java/com/kita/monstercorp/StartScreen.kt
@@ -1,5 +1,6 @@
 package com.kita.monstercorp
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -8,14 +9,32 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun StartScreen(onClickGame: () -> Unit) {
+fun StartScreen(modifier: Modifier = Modifier, onClickGame: () -> Unit) {
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Column {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            Color(0xff06131D),
+                            Color(0xff071925),
+                            Color(0xff103550),
+                            Color(0xff1C5B85),
+                        )
+                    )
+                )
+                .padding(innerPadding)
+                .padding(vertical = 64.dp, horizontal = 16.dp)
+        ){
             Text(
                 text = "Start Screen",
-                modifier = Modifier.padding(innerPadding)
+                color = Color.White
             )
             Button(onClick = onClickGame) {
                 Text("Go to Game")
